ec7bfcb2a8eab53822d4f9381a50b17a42e02fc8
Samuel Calderon <samuelcalderon1202@hotmail.com>
2025-04-12 14:35:11 -0500

    CreacionExcelDimRaster2

--- Rasters/CargandoRasters/manejoTOC.py
+++ Rasters/CargandoRasters/manejoTOC.py
@@ -1,4 +1,4 @@
-import os
+from pathlib import Path
 import pandas as pd
 from qgis.PyQt.QtGui import QColor
 from qgis.core import (
@@ -18,14 +18,16 @@ def usigRasters(grupo, layers) -> None:
     # Este codigo es principalmente el utilizado para el uso de las capas raster cargadas anteriormente en loadingLayers
 
     # Creando carpeta donde se guardaran los datos obtenidos de las capas
-    os.makedirs('datosRasters',exist_ok=True)
+    carpetaPadre = Path.cwd().parent
+    carpetaDatos = carpetaPadre / "datosRasters"
+    carpetaDatos.mkdir(parents=True, exist_ok=True)
 
     # Obtener dimensiones del raster
-    dim = [(layer.width(), layer.height()) for layer in layers if layer.isValid()]
-    df_dim =  pd.DataFrame(dim, columns=["Ancho (px)","Alto(px)"])
+    dim = [[layer.name(),layer.width(), layer.height()] for layer in layers if layer.isValid()]
+    df_dim =  pd.DataFrame(dim, columns=["Bandas","Ancho (px)","Alto(px)"])
 
     # Guardando en excel
-    salidaExcel = os.path.join(os.getcwd(), 'datosRasters', 'dimRasters.xlsx')
+    salidaExcel = carpetaDatos / 'dimRasters.xlsx'
     df_dim.to_excel(salidaExcel, index=False)
     print(f'El excel con las dimensiones se ha guardado en: {salidaExcel}')
 
@@ -37,21 +39,23 @@ def loadingLayers(): # No se específica que devuelve la función para evitar fu
     """
 
     proyecto = QgsProject.instance()
-    if not os.path.exists('D:/Programacion/QGIS/Rasters/ImgsRaster'): #Verifica que la carpeta exista, si exite no pasa nada, si no, se acaba
-        print("La dirección de la carpeta no existe")
+    rutaCarpeta = Path('D:/Programacion/QGIS/Rasters/ImgsRaster')
+
+    if not rutaCarpeta.exists(): #Verifica que la carpeta exista, si exite no pasa nada, si no, se acaba
+        print("La dirección de las imagenes no existe")
         return
 
     archivos : list = [] #Se crea la lista de archivos con la finalidad de que si no existe nada permita culminar el código
 
     # Esta sección del código permite verificar si el usuario esta cargando correctamente los archivos que posteriormente se van a manejar
     try:
-        rutaCarpeta : str = 'D:/Programacion/QGIS/Rasters/ImgsRaster'
-        archivos : list = [os.path.join(rutaCarpeta,doc) for doc in os.listdir(rutaCarpeta) if doc.lower().endswith(('.tif','.tiff'))] #Permite unir el archivo raster con la ruta de la carpeta para futuros usos
+        archivos : list = [doc for doc in rutaCarpeta.iterdir() if doc.suffix.lower() in ['.tif','.tiff']] #Permite unir el archivo raster con la ruta de la carpeta para futuros usos
         if not archivos:
             print("No se han encontrado archivos .tiff")
             return
     except Exception as ex:
         print(f"Ocurrio el siguiente error: {ex}")
+        return
 
     # Creando un grupo para todos los rasters cargados
     layersCargados = []
@@ -60,16 +64,31 @@ def loadingLayers(): # No se específica que devuelve la función para evitar fu
 
     # Cargando los rasters al proyecto iniciado
     for i, archivo in enumerate(archivos, start=1):
-        rlayer : QgsRasterLayer = QgsRasterLayer(archivo, f"Banda{i}")
-        if rlayer.isValid():
+
+        try:
+            #Obtiene el nombre de la capa basado en el archivo
+            nameLayer = archivo.stem
+            capasExistentes = {lyr.source(): lyr for lyr in proyecto.mapLayers().values()}
+            if str(archivo) in capasExistentes:
+                print(f'La imagen ya esta cargada')
+                continue
+
+            # Crear y validar capa
+            rlayer = QgsRasterLayer(str(archivo), nameLayer)
+            if not rlayer.isValid():
+                raise Exception(f"Archivo inválido: {nameLayer}")
+
+            # Añadir al proyecto y grupo
             proyecto.addMapLayer(rlayer)
             layersCargados.append(rlayer)
-            #Añadiendo el raster a la carpeta Rasters
+
+            # Mover al grupo
             nodo = root.findLayer(rlayer.id())
             rts.addChildNode(nodo.clone())
             root.removeChildNode(nodo)
-        else:
-            print(f"El archivo {archivo} no es valido")
+
+        except Exception as ex:
+            print(f"❌ Error procesando {archivo.name}: {ex}")
 
     return rts, layersCargados
 
@@ -90,6 +109,8 @@ def startingProject () -> None:
 
     except Exception as ex:
         print(f"Ocurrio el siguiente error: {ex}")
+        return
+
     finally:
         qgis.exitQgis()
 
